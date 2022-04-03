3d8a4af7bad1e98eea9ca17a10116fe92401fe88
gustavoalves23 <gustavo.alves388@gmail.com>
2022-04-03 18:18:09 -0300

    test: Tests to coffe-store page link to home page

--- pages/coffee-store/[id].jsx
+++ pages/coffee-store/[id].jsx
@@ -1,12 +1,15 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import styles from './style.module.css';
 
 function coffeeStore() {
 	const {query: {id}} = useRouter();
 	return (
-		<div>
-			<Link href='/'>Back to home!</Link>
+		<div className={styles.container} data-test='coffee-store-container'>
+			<Link href='/'>
+				<a className={styles.back_homepage_link}>Back to home!</a>
+			</Link>
       This is the coffee-store {id} route!
 		</div>
 	);
