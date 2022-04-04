ab0ad2bcfa7ca6f6b209745e777f869b94b83dd4
gustavoalves23 <gustavo.alves388@gmail.com>
2022-04-04 10:15:28 -0300

    test:Refactor github workflow to use jest

--- cypress/support/Utils/dataTest.js
+++
@@ -1,3 +0,0 @@
-export default function dataTest(data) {
-  return `[data-test=${data}]`;
-}
--- cypress/support/Utils/index.js
+++
@@ -1,4 +0,0 @@
-import dataTest from "./dataTest";
-import hexToRgb from "./HexToRGB";
-
-export { dataTest, hexToRgb };
