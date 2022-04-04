acb7533dbb997dc4e42ec06adcbe1e411c4251de
gustavoalves23 <gustavo.alves388@gmail.com>
2022-04-04 14:26:41 -0300

    fix: Home import tests

--- Tests/Homepage.test.js
+++ Tests/Homepage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Home from '../Pages/';
+import Home from '../pages';
 // import userEvent from '@testing-library/user-event';
 
 describe('Homepage', () => {
